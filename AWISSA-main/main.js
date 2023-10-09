1f2fa7797838f88cfabcfaabff6ab484deb4dd9b
lucasxu727 <124118546+lucasxu727@users.noreply.github.com>
2023-10-08 21:26:10 -0600

    Delete AWISSA-main directory

--- AWISSA-main/main.js
+++
@@ -1,9 +0,0 @@
-// Initialize the map
-var myMap = L.map("map").setView([51.505, -0.09], 13); // Coordinates: [latitude, longitude], Zoom level
-
-// Add a tile layer (OpenStreetMap)
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(myMap);
